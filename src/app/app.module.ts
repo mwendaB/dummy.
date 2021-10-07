634224069e868c565c8abd458046173ec403a165
mwendaB <brian.mwenda@moringaschool.com>
2021-10-07 13:29:51 +0300

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    GoalFormComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/goal.ts
@@ -0,0 +1,6 @@
+export class Goal {
+    showDescription: boolean;
+    constructor(,public name: string,public description: string, public completeDate: Date){
+      this.showDescription=false;
+    }
+}
